29f986d9247e40b008b61d63995cce0304d1cdb8
Mirko <mirkocaccavone03@gmail.com>
2025-05-02 18:22:39 +0200

    [feat] add CSS style

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ require_once "./functions.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>dischi json</title>
 </head>
 <body>
